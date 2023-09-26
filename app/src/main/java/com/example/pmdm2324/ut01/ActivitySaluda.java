3fd1df02e06aa24d104740609826a7d8718f886c
ErickLucano <ericlucano8@gmail.com>
2023-09-26 17:48:20 +0200

    mi primera app

---
+++ app/src/main/java/com/example/pmdm2324/ut01/ActivitySaluda.java
@@ -0,0 +1,16 @@
+package com.example.pmdm2324.ut01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm2324.R;
+
+public class ActivitySaluda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ut01_activity_saluda);
+    }
+}
